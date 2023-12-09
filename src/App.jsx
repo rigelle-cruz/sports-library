6f0d6a93bb6c0f2cfcf3aeacfe33bea28cfe3c6c
Rigelle Cruz <rigellecruz@gmail.com>
2023-12-10 12:02:26 +1300

    initialise project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SportsList from './components/SportsList';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Sports Library</h1>
+			<SportsList />
+		</div>
+	);
+}
+
+export default App;
