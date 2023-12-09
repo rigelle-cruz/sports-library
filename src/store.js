24d6a860d865fc216481b8b5e11ca9a0a414c145
Rigelle Cruz <rigellecruz@gmail.com>
2023-12-10 12:48:50 +1300

    add sportsSlice to store

--- src/store.js
+++ src/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import sportsSlice from './slices/sportsSlice';
 
 export const store = configureStore({
   reducer: {
-  
+    reducer: {
+      sports: sportsSlice
+    }
   },
 });
